9ae80b093021531266ac117616fff2fa76d8e652
sabaambr <sa6143@nyu.edu>
2021-02-24 22:40:41 -0500

    first commit

--- solution.py
+++ solution.py
@@ -13,7 +13,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
     clientSocket = socket(AF_INET, SOCK_STREAM)
 
-    clientSocket.connect((mailserver, 25))
+    clientSocket.connect((mailserver, 1025))
 
     recv = clientSocket.recv(1024).decode()
     #print(recv)
